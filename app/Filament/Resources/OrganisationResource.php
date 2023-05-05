6e84ed379c80f16bf81c70cf5bdcf1c9568df604
Lupu Gheorghe <gheorghelupu17@gmail.com>
2023-05-05 11:31:48 +0300

    fix order

--- app/Filament/Resources/OrganisationResource.php
+++ app/Filament/Resources/OrganisationResource.php
@@ -271,7 +271,7 @@ class OrganisationResource extends Resource
             ])
             ->bulkActions([
                 Tables\Actions\DeleteBulkAction::make(),
-            ]);
+            ])->defaultSort('created_at', 'desc');
     }
 
     public static function getRelations(): array
