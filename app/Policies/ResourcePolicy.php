09b231d86cdcc8218f1cdd33fb3a990a21c12a7f
Andrei Ioniță <hi@andrei.io>
2023-07-12 18:13:40 +0100

    wip

---
+++ app/Policies/ResourcePolicy.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Policies;
+
+use App\Models\Resource;
+use App\Models\User;
+
+class ResourcePolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Resource $resource): bool
+    {
+        return $user->isPlatformAdmin();
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Resource $resource): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Resource $resource): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Resource $resource): bool
+    {
+        return $user->isPlatformAdmin();
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Resource $resource): bool
+    {
+        return $user->isPlatformAdmin();
+    }
+}
