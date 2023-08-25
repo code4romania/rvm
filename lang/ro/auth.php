2db75efb4f09a87a79d33185696228e6ed61c866
Andrei Ioniță <hi@andrei.io>
2023-08-25 20:26:31 +0300

    chore: update dependencies (#98)
    * chore: update dependencies
    
    * chore: update missing translations

--- lang/ro/auth.php
+++ lang/ro/auth.php
@@ -3,20 +3,17 @@
 declare(strict_types=1);
 
 return [
-
-    'failed' => 'Datele de identificare nu pot fi confirmate.',
-    'password' => 'Parola introdusă este incorrectă.',
-    'throttle' => 'Prea multe încercări de intrare în cont. Poți încerca din nou peste :seconds secunde.',
-
+    'failed'                => 'Datele de identificare nu pot fi confirmate.',
     'organisation_inactive' => 'Organizația este inactivă.',
-
-    'welcome' => [
-        'subject' => 'Invitație la platforma RVM',
-        'intro_line_1' => 'Ești invitat să te alături platformei RVM - Sistemul de management de resurse umane și materiale în cazul unui dezastru, administrat de Departamentul Situații de Urgență al României.',
-        'intro_line_2' => 'Acceptă invitația pentru a-ți putea accesa contul.',
-        'submit' => 'Acceptă invitația',
+    'password'              => 'Parola este greșită.',
+    'throttle'              => 'Prea multe încercări de intrare în cont. Puteți încerca din nou peste :seconds secunde.',
+    'welcome'               => [
+        'already_used'      => 'Acest link a fost folosit deja.',
+        'intro_line_1'      => 'Ești invitat să te alături platformei RVM - Sistemul de management de resurse umane și materiale în cazul unui dezastru, administrat de Departamentul Situații de Urgență al României.',
+        'intro_line_2'      => 'Acceptă invitația pentru a-ți putea accesa contul.',
         'invalid_signature' => 'Acest link nu are o semnătură validă.',
-        'no_user' => 'Acest utilizator nu este valid.',
-        'already_used' => 'Acest link a fost folosit deja.',
+        'no_user'           => 'Acest utilizator nu este valid.',
+        'subject'           => 'Invitație la platforma RVM',
+        'submit'            => 'Acceptă invitația',
     ],
 ];
