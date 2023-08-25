5e9b6545269f45027cee70fe6e52c5c5f905f8d9
Andrei Ioniță <hi@andrei.io>
2023-08-25 18:24:46 +0100

    feat: add county level filtering to the platform stats widget

--- app/Filament/Widgets/PlatformStatsWidget.php
+++ app/Filament/Widgets/PlatformStatsWidget.php
@@ -8,15 +8,40 @@ use App\Filament\Resources\OrganisationResource;
 use App\Filament\Resources\ResourceResource;
 use App\Filament\Resources\UserResource;
 use App\Filament\Resources\VolunteerResource;
+use App\Models\County;
 use App\Models\Organisation;
 use App\Models\Resource;
 use App\Models\User;
 use App\Models\Volunteer;
+use Filament\Forms\Components\Select;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Card;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Collection;
 
 class PlatformStatsWidget extends BaseWidget
 {
+    protected static string $view = 'filament.widgets.platform-stats-widget';
+
+    /**
+     * County id to filter by.
+     *
+     * @var null|string
+     */
+    public string $county = '';
+
+    /**
+     * Available counties to select.
+     *
+     * @var \Illuminate\Support\Collection
+     */
+    public Collection $counties;
+
+    public function mount()
+    {
+        $this->counties = County::all();
+    }
+
     public static function canView(): bool
     {
         return auth()->user()->isPlatformAdmin()
@@ -26,21 +51,57 @@ class PlatformStatsWidget extends BaseWidget
     protected function getCards(): array
     {
         return [
-            Card::make(__('organisation.label.plural'), Organisation::count())
+            Card::make(__('organisation.label.plural'), $this->getOrganisationCount())
                 ->icon('heroicon-o-office-building')
                 ->url(OrganisationResource::getUrl('index')),
 
-            Card::make(__('resource.label.plural'), Resource::count())
+            Card::make(__('resource.label.plural'), $this->getResourceCount())
                 ->icon('heroicon-o-collection')
                 ->url(ResourceResource::getUrl('index')),
 
-            Card::make(__('volunteer.label.plural'), Volunteer::count())
+            Card::make(__('volunteer.label.plural'), $this->getVolunteerCount())
                 ->icon('heroicon-o-user-group')
                 ->url(VolunteerResource::getUrl('index')),
 
-            Card::make(__('user.label.plural'), User::count())
+            Card::make(__('user.label.plural'), $this->getUserCount())
                 ->icon('heroicon-o-office-building')
                 ->url(UserResource::getUrl('index')),
         ];
+    }
+
+    protected function getOrganisationCount(): int
+    {
+        return Organisation::query()
+            ->when($this->county, function (Builder $query) {
+                $query->where('county_id', $this->county);
+            })
+            ->count();
+    }
+
+    public function getResourceCount(): int
+    {
+        return Resource::query()
+            ->when($this->county, function (Builder $query) {
+                $query->where('county_id', $this->county);
+            })
+            ->count();
+    }
+
+    public function getVolunteerCount(): int
+    {
+        return Volunteer::query()
+            ->when($this->county, function (Builder $query) {
+                $query->where('county_id', $this->county);
+            })
+            ->count();
+    }
+
+    public function getUserCount(): int
+    {
+        return User::query()
+            ->when($this->county, function (Builder $query) {
+                $query->whereRelation('organisation', 'county_id', $this->county);
+            })
+            ->count();
     }
 }
