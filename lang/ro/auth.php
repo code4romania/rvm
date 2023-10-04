6d2302189f2d86e42253fc832b5f503f68f24599
Andrei Ioniță <hi@andrei.io>
2023-10-04 16:08:00 +0100

    fix: change welcome page title

--- lang/ro/auth.php
+++ lang/ro/auth.php
@@ -3,17 +3,18 @@
 declare(strict_types=1);
 
 return [
-    'failed'                => 'Datele de identificare nu pot fi confirmate.',
+    'failed' => 'Datele de identificare nu pot fi confirmate.',
     'organisation_inactive' => 'Organizația este inactivă.',
-    'password'              => 'Parola este greșită.',
-    'throttle'              => 'Prea multe încercări de intrare în cont. Puteți încerca din nou peste :seconds secunde.',
-    'welcome'               => [
-        'already_used'      => 'Acest link a fost folosit deja.',
-        'intro_line_1'      => 'Ești invitat să te alături platformei RVM - Sistemul de management de resurse umane și materiale în cazul unui dezastru, administrat de Departamentul Situații de Urgență al României.',
-        'intro_line_2'      => 'Acceptă invitația pentru a-ți putea accesa contul.',
+    'password' => 'Parola este greșită.',
+    'throttle' => 'Prea multe încercări de intrare în cont. Puteți încerca din nou peste :seconds secunde.',
+    'welcome' => [
+        'already_used' => 'Acest link a fost folosit deja.',
+        'intro_line_1' => 'Ești invitat să te alături platformei RVM - Sistemul de management de resurse umane și materiale în cazul unui dezastru, administrat de Departamentul Situații de Urgență al României.',
+        'intro_line_2' => 'Acceptă invitația pentru a-ți putea accesa contul.',
         'invalid_signature' => 'Acest link nu are o semnătură validă.',
-        'no_user'           => 'Acest utilizator nu este valid.',
-        'subject'           => 'Invitație la platforma RVM',
-        'submit'            => 'Acceptă invitația',
+        'no_user' => 'Acest utilizator nu este valid.',
+        'subject' => 'Invitație la platforma RVM',
+        'accept_invitation' => 'Acceptă invitația',
+        'submit' => 'Setează parola',
     ],
 ];
