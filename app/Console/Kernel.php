77c3dcfc187709ef8b95be88b33af3de5fcb9d81
Andrei Ioniță <hi@andrei.io>
2024-01-11 10:45:05 +0000

    fix: scheduled job monitor

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
+     * @param  Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
@@ -23,7 +23,7 @@ class Kernel extends ConsoleKernel
             ->at('06:00')
             ->timezone('Europe/Bucharest')
             ->withoutOverlapping()
-            ->sentryMonitor();
+            ->sentryMonitor('process-protocols-job');
     }
 
     /**
@@ -34,7 +34,5 @@ class Kernel extends ConsoleKernel
     protected function commands()
     {
         $this->load(__DIR__ . '/Commands');
-
-        require base_path('routes/console.php');
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         $this->registerCarbonMacros();
+
+        if ($this->app->isLocal()) {
+            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+            $this->app->register(TelescopeServiceProvider::class);
+        }
     }
 
     /**
