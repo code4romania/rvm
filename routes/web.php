6443d92ae1636185913b0ab1d1f489b72cfe396b
Andrei Ioniță <hi@andrei.io>
2025-05-26 14:02:36 +0100

    fix: route login not defined (#191)

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Fix for the `Route [login] not defined.` error that keeps appearing in development.
+Route::get('/laravel/login', fn () => redirect(route('filament.auth.login')))->name('login');
