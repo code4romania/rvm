ad9352e91c50fc0ae7f75c538394dc76abae1f55
Ion Dormenco <idormenco@gmail.com>
2025-06-12 12:55:37 +0300

    Add filtering by published at (#194)
    * Add filtering by published at
    
    * Update NewsController.php
    
    * style: apply fix
    
    ---------
    
    Co-authored-by: Andrei Ioniță <hi@andrei.io>

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Http\Filters\PublishedAfterFilter;
+use App\Http\Filters\PublishedAtFilter;
+use App\Http\Filters\PublishedBeforeFilter;
 use App\Http\Resources\NewsResource;
 use App\Models\News;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Spatie\QueryBuilder\AllowedFilter;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class NewsController extends Controller
 {
@@ -16,16 +20,22 @@ class NewsController extends Controller
         $this->authorize('accessApi');
 
         return NewsResource::collection(
-            News::query()
-                ->wherePublished()
+            QueryBuilder::for(News::class)
+                ->allowedFilters([
+                    AllowedFilter::custom('published_at', new PublishedAtFilter),
+                    AllowedFilter::custom('published_before', new PublishedBeforeFilter),
+                    AllowedFilter::custom('published_after', new PublishedAfterFilter),
+                ])
                 ->with([
                     'media',
-                    'organisation' => fn(BelongsTo $query) => $query
+                    'organisation' => fn ($query) => $query
                         ->withoutEagerLoads()
                         ->select('id', 'name'),
                 ])
-                ->orderBy('id', 'desc')
+                ->where('status', 'published')
+                ->defaultSort('-published_at')
                 ->paginate(25)
+                ->appends(request()->query())
         );
     }
 }
---
+++ app/Http/Filters/PublishedAfterFilter.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Filters;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Carbon;
+use Spatie\QueryBuilder\Filters\Filter;
+
+class PublishedAfterFilter implements Filter
+{
+    public function __invoke(Builder $query, $value, string $property): Builder
+    {
+        try {
+            $date = Carbon::parse($value);
+        } catch (\Exception $e) {
+            return $query;
+        }
+
+        return $query->whereDate('published_at', '>=', $date);
+    }
+}
